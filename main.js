60bbf20208d00bbf98855cfca72c651532f47297
tarek2001harba <tarek.harba2001b@gmail.com>
2020-02-01 05:57:12 +0200

    Last massive update

--- main.js
+++ main.js
@@ -2,11 +2,18 @@ function prallax(element, distance, speed) {
   const item = document.querySelector(element);
   item.style.transform = `translateY(${distance * speed}px)`;
 }
-
+function negprallax(element, distance, speed) {
+  const item = document.querySelector(element);
+  item.style.transform = `translateY(-${distance * speed + 70}px)`;
+}
 window.addEventListener("scroll", function() {
   prallax(".c-one", window.scrollY, 0.5);
   prallax(".c-two", window.scrollY, 0.4);
   prallax(".c-three", window.scrollY, 0.6);
+});
+window.addEventListener("scroll", function() {
+  negprallax(".s-one", window.scrollY, 0.4);
+  negprallax(".s-two", window.scrollY, 0.3);
 });
 
 window.addEventListener("load", () => {
